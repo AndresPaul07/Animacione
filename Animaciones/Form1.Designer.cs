e10577859ebdf5de7786fd6e88c18c928dcac73c
Paul <Paul@GWNC21524>
2023-11-21 05:19:04 -0500

    Agregar archivos de proyecto.

---
+++ Animaciones/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Animaciones
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.titulo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Si lo imaginas lo puedes programar";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Animaciones.Properties.Resources.jugar;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(25, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sonido";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.Location = new System.Drawing.Point(211, 38);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(111, 20);
+            this.titulo.TabIndex = 2;
+            this.titulo.Text = "Animaciones";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Animaciones.Properties.Resources.animacion;
+            this.pictureBox1.Location = new System.Drawing.Point(230, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 194);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Animaciones.Properties.Resources.naipes;
+            this.ClientSize = new System.Drawing.Size(509, 331);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.titulo);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Animaciones/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animaciones
+{
+    public partial class MainForm : Form
+    {
+        private int XPos = 0; // Posición inicial en el eje X
+        private SoundPlayer soundPlayer; // Declara un objeto SoundPlayer
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeAnimation();
+            InitializeSound(); // Inicializa el objeto SoundPlayer
+        }
+        private void InitializeAnimation()
+        {
+            // Configurar el temporizador para la animación
+            Timer animationTimer = new Timer();
+            animationTimer.Interval = 100; // Intervalo en milisegundos
+            animationTimer.Tick += timer1_Tick;
+            animationTimer.Start();
+        }
+        private void InitializeSound()
+        {
+            // Ruta del archivo de sonido (puedes cambiarlo a la ubicación de tu archivo de sonido)
+            string soundFilePath = @"C:\Users\Paul\Music\sonido.wav";
+            // Inicializa el objeto SoundPlayer con la ruta del archivo de sonido
+            soundPlayer = new SoundPlayer(soundFilePath);
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Puedes agregar otros elementos como gráficos, audio, etc., en este evento o en el diseñador
+            // Por ejemplo, aquí estoy agregando un Label para animar
+            System.Windows.Forms.Label label = new System.Windows.Forms.Label();
+            label.Text = "¡Animación!";
+            label.AutoSize = true;
+            label.Location = new System.Drawing.Point(0, 50);
+            this.Controls.Add(label);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Actualizar la posición del elemento animado (en este caso, un Label)
+            XPos += 5; // Incremento en la posición X
+            // Verificar si la animación ha llegado al final
+            if (XPos > this.Width)
+            {
+                XPos = 0; // Reiniciar la animación al llegar al final
+            }
+            // Actualizar la posición del elemento en la interfaz
+            label1.Location = new System.Drawing.Point(XPos, label1.Location.Y);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            soundPlayer.Play();
+        }
+    }
+}
